b949e7d98bae4403884b01019a6a8ccb08a8eea3
Leo Terziman <leo.terziman@clarte.asso.fr>
2019-04-05 12:11:09 +0200

    Simplified dictionary serialization to leverage new features of binary serializer

--- Sources/Model/Type/Binary/Metadata.cs
+++ Sources/Model/Type/Binary/Metadata.cs
@@ -7,46 +7,12 @@ namespace Armine.Model.Type
 	{
 		public uint FromBytes(Binary serializer, Binary.Buffer buffer, uint start)
 		{
-			uint size;
-			string key;
-			object value;
-
-			Initialize();
-
-			uint read = serializer.FromBytes(buffer, start, out size);
-
-			for(uint i = 0; i < size; ++i)
-			{
-				read += serializer.FromBytes(buffer, start + read, out key);
-				read += serializer.FromBytes(buffer, start + read, out value);
-
-				data.Add(key, value);
-			}
-
-			return read;
+			return serializer.FromBytes(buffer, start, out data);
 		}
 
 		public uint ToBytes(Binary serializer, ref Binary.Buffer buffer, uint start)
 		{
-			uint count = 0;
-
-			uint written = serializer.ToBytes(ref buffer, start, count); // placeholder
-
-			foreach(KeyValuePair<string, object> pair in data)
-			{
-				if(!string.IsNullOrEmpty(pair.Key))
-				{
-					written += serializer.ToBytes(ref buffer, start + written, pair.Key);
-					written += serializer.ToBytes(ref buffer, start + written, pair.Value != null ? pair.Value : "");
-
-					count++;
-				}
-			}
-
-			// Replace the count by the correct value
-			serializer.ToBytes(ref buffer, start, count);
-
-			return written;
+			return serializer.ToBytes(ref buffer, start, data);
 		}
 	}
 }
--- Sources/Model/Type/Binary/UnityComponent.cs
+++ Sources/Model/Type/Binary/UnityComponent.cs
@@ -17,8 +17,8 @@ namespace Armine.Model.Type
             {
                 uint read = 0;
 
-                read += ReadDictionary(serializer, buffer, start + read, ref fields);
-                read += ReadDictionary(serializer, buffer, start + read, ref properties);
+                read += serializer.FromBytes(buffer, start + read, out fields);
+                read += serializer.FromBytes(buffer, start + read, out properties);
 
                 return read;
             }
@@ -27,60 +27,8 @@ namespace Armine.Model.Type
             {
                 uint written = 0;
 
-                written += WriteDictionary(serializer, ref buffer, start + written, fields);
-                written += WriteDictionary(serializer, ref buffer, start + written, properties);
-
-                return written;
-            }
-            #endregion
-
-            #region Helper methods
-            private static uint ReadDictionary(Binary serializer, Binary.Buffer buffer, uint start, ref Dictionary<string, object> dictionary)
-            {
-                uint size;
-                string key;
-                object value;
-
-                uint read = serializer.FromBytes(buffer, start, out size);
-
-                if(size > 0)
-                {
-                    dictionary = new Dictionary<string, object>();
-                }
-
-                for(uint i = 0; i < size; ++i)
-                {
-                    read += serializer.FromBytes(buffer, start + read, out key);
-                    read += serializer.FromBytes(buffer, start + read, out value);
-
-                    dictionary.Add(key, value);
-                }
-
-                return read;
-            }
-
-            private static uint WriteDictionary(Binary serializer, ref Binary.Buffer buffer, uint start, Dictionary<string, object> dictionary)
-            {
-                uint count = 0;
-
-                uint written = serializer.ToBytes(ref buffer, start, count); // placeholder
-
-                if(dictionary != null)
-                {
-                    foreach(KeyValuePair<string, object> pair in dictionary)
-                    {
-                        if(!string.IsNullOrEmpty(pair.Key) && pair.Value != null)
-                        {
-                            written += serializer.ToBytes(ref buffer, start + written, pair.Key);
-                            written += serializer.ToBytes(ref buffer, start + written, pair.Value);
-
-                            count++;
-                        }
-                    }
-
-                    // Replace the count by the correct value
-                    serializer.ToBytes(ref buffer, start, count);
-                }
+                written += serializer.ToBytes(ref buffer, start + written, fields);
+                written += serializer.ToBytes(ref buffer, start + written, properties);
 
                 return written;
             }
